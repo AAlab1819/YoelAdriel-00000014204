bb13e39c8e089d4c0446d2ff4ed8d4f8c664ec3f
yoeladrielcandr <yoel.a.candra@gmail.com>
2018-10-24 21:46:33 +0700

    Update CouponsandDiscounts.cpp

--- Week7-Greedy/CouponsandDiscounts.cpp
+++ Week7-Greedy/CouponsandDiscounts.cpp
@@ -12,8 +12,6 @@ int main() {
     
     for(int i = 0; i < c; i++)
     	if(arr[i] % 2 == 0)
-    		continue;
-    	else
     		if(arr[i + 1]-- == 0) {
     			cout << "NO" << endl;
     			return 0;
